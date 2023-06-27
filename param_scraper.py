7fb611925f709fc19b0e57460cceeca467ebb7d5
Cameron Faith <127695511+encodexdev@users.noreply.github.com>
2023-06-27 12:54:34 -0600

    First working version

--- param_scraper.py
+++ param_scraper.py
@@ -11,16 +11,18 @@ def scan_ipynb_for_params(ipynb_path, json_path):
             lines = cell['source'].split('\n')
             for line in lines:
                 line = line.strip()
-                if line.startswith('#') or not line:
-                    continue
-                if '@param' in line:
-                    parts = line.split('@param')[1].split('#')[0].strip().split('=')
-                    param_name = parts[0].strip()
-                    param_value = parts[1].strip()
-                    params[param_name] = param_value
+                if '@param' in line and not line.startswith('#'):
+                    print(line)
+                    parts = line.split('#')[0].strip().split('=')
+                    print(parts)
+                    if len(parts) >= 2:
+                        param_name = parts[0].strip()
+                        param_value = parts[1].strip().split()[0]  # Extract the parameter value
+                        params[param_name] = param_value
 
     with open(json_path, 'w', encoding='utf-8') as file:
         json.dump(params, file, indent=4)
 
 # Example usage
-scan_ipynb_for_params('path/to/your/notebook.ipynb', 'params.json')
+scan_ipynb_for_params('./Disco_Diffusion.ipynb', 'params.json')
+
