7b31f83d9c647eb2e88abf8f2041f8971158531b
Cameron Faith <127695511+encodexdev@users.noreply.github.com>
2023-06-27 13:26:57 -0600

    Fix formatting

--- param_scraper.py
+++ param_scraper.py
@@ -1,6 +1,34 @@
 import json
 import nbformat
 
+def extract_param_value(value_str, param_type):
+    if param_type.isspace():
+        return value_str
+
+    # Detect the type based on patterns in param_type
+    param_type = param_type.lower()
+    if 'number' in param_type or 'float' in param_type or 'int' in param_type:
+        try:
+            return int(value_str)
+        except ValueError:
+            try:
+                return float(value_str)
+            except ValueError:
+                return value_str
+    elif 'boolean' in param_type:
+        return value_str.lower() == 'true'
+    elif 'string' in param_type:
+        if value_str.startswith('"') and value_str.endswith('"'):
+            return value_str[1:-1]
+        elif value_str.startswith("'") and value_str.endswith("'"):
+            return value_str[1:-1]
+        else:
+            return value_str
+    else:
+        return value_str
+
+
+
 def scan_ipynb_for_params(ipynb_path, json_path):
     with open(ipynb_path, 'r', encoding='utf-8') as file:
         notebook = nbformat.read(file, as_version=4)
@@ -12,16 +40,18 @@ def scan_ipynb_for_params(ipynb_path, json_path):
             for line in lines:
                 line = line.strip()
                 if '@param' in line and not line.startswith('#'):
-                    print(line)
-                    parts = line.split('#')[0].strip().split('=')
-                    print(parts)
+                    split_line = line.strip().split('#')
+                    parts = split_line[0].split('=')
                     if len(parts) >= 2:
                         param_name = parts[0].strip()
-                        param_value = parts[1].strip().split()[0]  # Extract the parameter value
+                        param_value_str = parts[1].strip()  # Extract the parameter value
+                        param_value = extract_param_value(param_value_str, split_line[1].strip("@param"))
                         params[param_name] = param_value
+                         
 
     with open(json_path, 'w', encoding='utf-8') as file:
         json.dump(params, file, indent=4)
+        # print(params)
 
 # Example usage
 scan_ipynb_for_params('./Disco_Diffusion.ipynb', 'params.json')
